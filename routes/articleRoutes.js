ed6994df8f4df32a711f2d6fc27e4eeb45664621
Viktor <riazhen.viktor1124@vu.cdu.edu.ua>
2024-11-12 21:04:48 +0200

    fix controller

--- routes/articleRoutes.js
+++ routes/articleRoutes.js
@@ -3,11 +3,15 @@ const router = express.Router();
 const articleController = require("../controllers/articleController");
 
 router.get("/",articleController.getArticles);
-router.get("/:id",articleController.getArticle);
 router.post("/create",articleController.createArticle);
 
-// for static pages
+
 router.get("/about", articleController.getAbout);
 router.get("/privacyPolicy", articleController.getPrivacyPolicy);
+
+
+
+router.get("/:id",articleController.getArticle);
+// router.get("/areicles/:id",articleController.getArticle);
 
 module.exports = router;
